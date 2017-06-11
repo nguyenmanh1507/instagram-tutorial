790d345a62658d36e636d3c154766604f91d494b
nguyenmanh1507 <nguyenmanh1507@gmail.com>
2017-06-12 00:02:27 +0700

    init

---
+++ src/ListPage.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import {
+  createFragmentContainer,
+  graphql
+} from 'react-relay'
+import Post from './Post'
+
+class ListPage extends Component {
+  render() {
+    return (
+      <div className="w-100 flex justify-center">
+        <div className="w-100" style={{maxWidth: 400}}>
+          {this.props.viewer.allPosts.edges.map(({ node }) => (
+              <Post key={node.__id} post={node} />
+            ))}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default createFragmentContainer(
+  ListPage,
+  graphql`
+    fragment ListPage_viewer on Viewer {
+      allPosts(last: 100, orderBy: createdAt_DESC) @connection(key: "ListPage_allPosts", filters: []) {
+        edges {
+          node {
+            ...Post_post
+          }
+        }
+      }
+    }
+  `
+)
